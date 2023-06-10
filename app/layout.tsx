894abfdabc550963cbdce457e8fe6a1acca28652
Krinzal <118538166+Krin-Zal@users.noreply.github.com>
2023-06-10 20:06:45 +0545

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} min-h-screen min-w-screen p-8`}>{children}</body>
     </html>
   )
 }
